68ffdd32ca6a989d96e7ac50c535d82d2e81a318
VIKING <viking.yfy@qq.com>
2024-10-14 16:24:25 +0800

    Merge QCA Source

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -37,6 +37,13 @@ platform_do_upgrade() {
 		fw_setenv bootcount 0
 		nand_do_upgrade "$1"
 		;;
+	cmiot,ax18|\
+	glinet,gl-ax1800|\
+	glinet,gl-axt1800|\
+	qihoo,v6|\
+	redmi,ax5|\
+	xiaomi,ax1800|\
+	zn,m2|\
 	netgear,wax214)
 		nand_do_upgrade "$1"
 		;;
@@ -50,6 +57,27 @@ platform_do_upgrade() {
 		fi
 		fw_setenv owrt_bootcount 0
 		fw_setenv owrt_slotactive $((1 - active))
+		nand_do_upgrade "$1"
+		;;
+	jdcloud,ax1800-pro|\
+	jdcloud,ax6600|\
+	redmi,ax5-jdcloud)
+		kernelname="0:HLOS"
+		rootfsname="rootfs"
+		mmc_do_upgrade "$1"
+		;;
+	linksys,mr7350)
+		boot_part="$(fw_printenv -n boot_part)"
+		if [ "$boot_part" -eq "1" ]; then
+			fw_setenv boot_part 2
+			CI_KERNPART="alt_kernel"
+			CI_UBIPART="alt_rootfs"
+		else
+			fw_setenv boot_part 1
+			CI_UBIPART="rootfs"
+		fi
+		fw_setenv boot_part_ready 3
+		fw_setenv auto_recovery yes
 		nand_do_upgrade "$1"
 		;;
 	*)
